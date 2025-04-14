d3d95413b7ff9a09ee39f7f871f53fe8d18ee874
Travis Luckenbaugh <tluckenbaugh@gmail.com>
2025-04-14 16:33:00 -0400

    Rest of data structure (unsafe, do not run)

--- cmd/dayone.go
+++ cmd/dayone.go
@@ -20,6 +20,49 @@ type JournalEntry struct {
     IsAllDay bool `json:"isAllDay"`
     Starred bool `json:"starred"`
     Text string `json:"text"`
+    Location JournalLocation `json:"location"`
+    TimeZone string `json:"timeZone"`
+    Duration float64 `json:"duration"`
+    Uuid string `json:"uuid"`
+    IsPinned bool `json:"isPinned"`
+    ModifiedDate string `json:"modifiedDate"`
+    Weather JournalWeather `json:"weather"`
+    CreationDate string `json:"creationDate"`
+
+}
+
+type JournalLocation struct {
+    Region JournalRegion `json:"region"`
+    Country string `json:"country"`
+    Longitude float64 `json:"longitude"`
+    AdministrativeArea string `json:"administrativeArea"`
+    PlaceName string `json:"placeName"`
+    Latitude float64 `json:"latitude"`
+}
+
+type JournalRegion struct {
+    Center JournalCenter `json:"center"`
+    Radius float64 `json:"radius"`
+}
+
+type JournalCenter struct {
+    Longitude float64 `json:"longitude"`
+    Latitude float64 `json:"latitude"`
+}
+
+type JournalWeather struct {
+    SunsetDate string `json:"sunsetDate"`
+    TemperatureCelsius float64 `json:"temperatureCelsius"`
+    WeatherServiceName string `json:"weatherServiceName"`
+    WindBearing float64 `json:"windBearing"`
+    SunriseDate string `json:"sunriseDate"`
+    ConditionsDescription string `json:"conditionsDescription"`
+    PressureMB float64 `json:"pressureMB"`
+    VisibilityKM float64 `json:"visibilityKM"`
+    RelativeHumidity float64 `json:"relativeHumidity"`
+    WindSpeedKPH float64 `json:"windSpeedKPH"`
+    WeatherCode string `json:"weatherCode"`
+    WindChillCelsius float64 `json:"windChillCelsius"`
 }
 
 func main() {
